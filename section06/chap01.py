b0b744aa96772da490e2b15bae5027f7dff0e59a
namhun4654 <namhun4654@gmail.com>
2024-01-28 14:58:51 +0900

    f

--- section06/chap01.py
+++ section06/chap01.py
@@ -9,4 +9,13 @@ while 논리식:
 
 i = 0
 while i < 10:
-    print('무한반복')
+    print(f'반복{i}')
+    i += 1
+
+print(f'while 반복문이 종료되었습니다, i={i}')
+
+until = int(input('반복 횟수를 입력하세요 >>> '))
+i = 0
+while i < until:
+    print(f'{i} / {until} 반복중...')
+    i += 1
